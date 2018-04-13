8f2746060189ca487dee63e083d12c5298ca5e45
Orvin Welchez <owelchez@gmail.com>
2018-04-13 09:19:58 -0400

    Getting things ready

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var env = require("dotenv").load();
+var models = require("./app/models");
+
+// Make sure your db connection works
+models.sequelize.sync().then(function() {
+	console.log('Database connection successful!');
+}).catch(function(err) {
+	console.log(err, 'You messed this one up real bad!');
+});
+
+// This will extract the body in a request
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Passport authentication 
+app.use(session({	secret: 'mazinger z',
+					resave: true, // Expiration dates...set to true
+					saveUninitialized: true	}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function(req, res) {
+	res.send('Welcome to my realm suckers!');
+});
+
+app.listen(5000, function(err) {
+	if (!err) {
+		console.log('Listening to your beat! Server running');
+	} else {
+		console.log(err);
+	}
+})
